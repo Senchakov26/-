78abe1c5eb2895aab975c0f7efd177fc3c49976f
Serg <Serg@DESKTOP-BN834QL>
2022-03-28 01:36:10 +0300

    sess1

---
+++ WindowsFormsAppTrening/WindowsFormsAppTrening/Classes/ClassAutorizate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppTrening.Classes
+{
+    class ClassAutorizate
+    {
+        static public int Auth(string login, string pass)
+        {
+            DBConnection.myCommand.CommandText = $@"select FioMan from manager where LoginMan = '{login}' and Password = md5('{pass}')";
+            if(DBConnection.myCommand.ExecuteScalar()!=null)
+            {
+                return 1;
+            }
+            else
+            {
+                DBConnection.myCommand.CommandText = $@"select FioExec from executer where LoginExec = '{login}' and Password = md5('{pass}')";
+                if (DBConnection.myCommand.ExecuteScalar() != null)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsAppTrening/WindowsFormsAppTrening/Classes/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsAppTrening.Classes
+{
+    class DBConnection
+    {
+        static public string connectStr = "Database = esoft; DataSource = localhost; user = root; password = qwerty; charset = utf8;";
+
+        static public MySqlConnection myConnect;
+        static public MySqlCommand myCommand;
+        static public MySqlDataAdapter myDataAdapter;
+
+        static public bool DBConnect()
+        {
+            myConnect = new MySqlConnection(connectStr);
+            myConnect.Open();
+            myCommand = new MySqlCommand();
+            myCommand.Connection = myConnect;
+            myDataAdapter = new MySqlDataAdapter(myCommand);
+            return true;
+        }
+        static public void DBConnectClose()
+        {
+            myConnect.Close();
+        }
+    }
+}
